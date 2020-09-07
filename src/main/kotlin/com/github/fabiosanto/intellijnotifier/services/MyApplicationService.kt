e6828fd5bfcf3165b40a495c2f88541435de96fc
GitHub Action <action@github.com>
2020-09-07 07:03:02 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/fabiosanto/intellijnotifier/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.fabiosanto.intellijnotifier.services
+
+import com.github.fabiosanto.intellijnotifier.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
